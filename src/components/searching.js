ed89c2fbf7783fd5048e0901c7fb9895e6f75bd0
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2025-12-26 19:12:19 +0300

    Update searching.js

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,12 +1,11 @@
-import {rules, createComparison} from "../lib/compare.js";
-
-export function initSearching(searchField) {
-    const compare = createComparison(
-        ['skipEmptyTargetValues'],
-        [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)]
-    );
-
-    return (data, state, action) => {
-        return data.filter(row => compare(row, state));
-    };
+export function initSearching(searchField, fieldName = "search") {
+  return (query, state, action) => {
+    // result заменили на query
+    return state[fieldName]
+      ? Object.assign({}, query, {
+          // проверяем, что в поле поиска было что-то введено
+          search: state[fieldName], // устанавливаем в query параметр
+        })
+      : query; // если поле с поиском пустое, просто возвращаем query без изменений
+  };
 }
